51d71422ca25b0c0422e10a5cb7802ffa228344c
lukaszklein <32834481+lukaszklein@users.noreply.github.com>
2017-12-11 12:37:28 +0100

    Add files via upload

--- CRC/CRC/Program.cs
+++ CRC/CRC/Program.cs
@@ -10,9 +10,9 @@ namespace kontrolaparzystosci
     {
         public static void Main(string[] args)
         {
-            long dzielnik = 0b10100000;
+            long dzielnik = 0b1010000000;
             long warunek = 0;
-            long maska = 0b10000000;
+            long maska = 0b1000000000;
             long bajt = 0b0;
             bool poczatek = true;
             Console.WriteLine("Podaj dane:");
@@ -22,58 +22,18 @@ namespace kontrolaparzystosci
             StringBuilder binaryStringBuilder = new StringBuilder();
             foreach (var b in buf)
             {
-                if (!poczatek)//przesunięcie przy obecności większej niż 1 liczby znaków
+                if (poczatek)//przepisanie przy pierwszym znaku
                 {
-                    bajt <<= 8;//przesuniecie reszty z poprzedniego dzielenia
-                    maska <<= 8;
-                    dzielnik = 0b101;//ze względu na wypchnięcie jednej z jedynek poza zakres, trzeba ponownie przypisać wartość dzielnika
-                    dzielnik <<= 7;
-                }
-                bajt += b;//przypisanie odczytanego znaku do zmiennej bajt poprzez dodanie jej do obecnej wartości bajta
-                if (poczatek)//obliczenie reszty z dzielenia
-                {
-                    for (int i = 0; i < 6; i++)
-                    {
-
-                        warunek = maska & bajt;
-                        if (warunek > 0)
-                        {
-                            bajt ^= dzielnik;
-                            maska = maska >> 1;
-                            dzielnik = dzielnik >> 1;
-                        }
-                        else
-                        {
-                            maska = maska >> 1;
-                            dzielnik = dzielnik >> 1;
-                        }
-                    }
+                    bajt = b;
                     poczatek = false;
                 }
-                else//dzielenie dla nastepnych znakow
+                else//wykonanie xor dla każdego następnego bajtu z poprzednimi
                 {
-                    for (int i = 0; i < 8; i++)
-                    {
-
-                        warunek = maska & bajt;
-                        if (warunek > 0)
-                        {
-                            bajt ^= dzielnik;
-                            maska = maska >> 1;
-                            dzielnik = dzielnik >> 1;
-                        }
-                        else
-                        {
-                            maska = maska >> 1;
-                            dzielnik = dzielnik >> 1;
-                        }
-                    }
+                    bajt ^= b;
                 }
             }
-            bajt <<= 2;//dodanie zer na końcu
-            maska <<= 2;
-            dzielnik = 0b1010;//przypisanie wartosci dzielnika
-            for (int i = 0; i < 2; i++)//obliczanie crc
+            bajt <<= 2;//dodanie dwóch zer 
+            for (int i = 0; i < 8; i++)//operacja dzielenia
             {
                 warunek = maska & bajt;
                 if (warunek > 0)
